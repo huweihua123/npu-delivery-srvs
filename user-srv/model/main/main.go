89e5bf5ed2ff76f3619f64bb8b173313825496ee
weihua <2215629678@qq.com>
2024-11-24 19:12:08 +0800

    chore(user-srv): add logging to user model operations

--- user-srv/model/main/main.go
+++ user-srv/model/main/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: weihua hu
  * @Date: 2024-11-24 15:21:44
- * @LastEditTime: 2024-11-24 15:24:52
+ * @LastEditTime: 2024-11-24 19:08:16
  * @LastEditors: weihua hu
  * @Description:
  */
@@ -9,21 +9,40 @@
 package main
 
 import (
+	"log"
 	"npu-delivery-srvs/user-srv/model"
+	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 func main() {
 	dsn := "root:123456hwh@tcp(127.0.0.1:3306)/npu_delivery_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // 禁用彩色打印
+		},
+	)
 
+	// 全局模式
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
 	if err != nil {
-		panic("failed to connect database")
+		panic(err)
 	}
 
-	// Migrate the schema
-	db.AutoMigrate(&model.User{})
+	_ = db.AutoMigrate(&model.User{})
+
 }
